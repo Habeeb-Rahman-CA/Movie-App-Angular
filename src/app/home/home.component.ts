f142e30bfb9dcd1e22db956aef0a5a2e6824fc45
HabeebR07 <habeebrahmanca07@gmail.com>
2024-10-29 23:07:44 +0530

    header

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,42 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  email = ""
+  password = ""
+  errorMsg = ""
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  login() {
+    if (this.email.trim().length === 0) {
+      this.errorMsg = "Email is required";
+      console.log("Email is required");
+    } else if (this.password.trim().length === 0) {
+      this.errorMsg = "Password is required";
+      console.log("Password is required");
+    } else {
+      this.errorMsg = ""
+      console.log("Logged In");
+      let response = this.authService.login(this.email, this.password)
+      if (response === 200) {
+        this.router.navigate(['home'])
+      }
+      if (response === 403) {
+        this.errorMsg = "Invalid account!"
+      }
+    }
+  }
 
 }
