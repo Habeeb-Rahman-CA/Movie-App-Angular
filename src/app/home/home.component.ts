5ed0607cf92f5555d81383d0f3db1ccd522e4a07
HabeebR07 <habeebrahmanca07@gmail.com>
2024-10-30 13:18:14 +0530

    add trending movies

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { HttpClient } from '@angular/common/http';
+import { trendingMovies } from '../../assets/data/trending-movies';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +11,15 @@ import { HeaderComponent } from '../header/header.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  getTrendingMovies = trendingMovies
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.getTrendingMovies
+    console.log(this.getTrendingMovies);
+  }
+  
 }
